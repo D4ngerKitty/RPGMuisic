7be13063e218c5707b80beef6835cf7e5efe2720
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2021-11-12 18:31:09 -0600

    Update Music.ts

--- Music.ts
+++ Music.ts
@@ -43,37 +43,48 @@ namespace CatQuestVo1 {
     music.playTone(659, music.beat(BeatFraction.Half))
     music.rest(music.beat(BeatFraction.Whole))
     }
+}
 
-    
+
+
+namespace CatQuestVo1 {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     <i class ="far fa-play-circle"></i>
+     */
+    //% block
     export function Play_song_Tavern() {
 
-            for (let index = 0; index < 1; index++) {
-                timer.background(function () {
-                    music.playTone(262, music.beat(BeatFraction.Quarter))
-                })
-                music.playTone(330, music.beat(BeatFraction.Quarter))
-                music.rest(music.beat(BeatFraction.Half))
-                music.playTone(349, music.beat(BeatFraction.Quarter))
-                music.playTone(330, music.beat(BeatFraction.Quarter))
+        for (let index = 0; index < 1; index++) {
+            timer.background(function () {
                 music.playTone(262, music.beat(BeatFraction.Quarter))
-                music.playTone(294, music.beat(BeatFraction.Quarter))
-                music.playTone(330, music.beat(BeatFraction.Quarter))
+            })
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Half))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            timer.background(function () {
                 music.playTone(262, music.beat(BeatFraction.Quarter))
-                music.rest(music.beat(BeatFraction.Half))
-                music.playTone(330, music.beat(BeatFraction.Quarter))
-                music.playTone(262, music.beat(BeatFraction.Quarter))
-                music.playTone(294, music.beat(BeatFraction.Quarter))
-                timer.background(function () {
-                    music.playTone(262, music.beat(BeatFraction.Quarter))
-                })
-                music.playTone(330, music.beat(BeatFraction.Quarter))
-                music.playTone(349, music.beat(BeatFraction.Quarter))
-                music.playTone(330, music.beat(BeatFraction.Quarter))
-                music.playTone(262, music.beat(BeatFraction.Quarter))
-                music.playTone(294, music.beat(BeatFraction.Quarter))
-                music.playTone(330, music.beat(BeatFraction.Quarter))
-                music.playTone(262, music.beat(BeatFraction.Quarter))
-                music.rest(music.beat(BeatFraction.Half))
+            })
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Half))
         }
     }
 }
