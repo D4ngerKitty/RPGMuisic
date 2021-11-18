74cbc3f27899390b24ede2562c204adeb57bbbe0
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2021-11-17 18:34:35 -0600

    Update pxt.json, CatquestVol2.ts

---
+++ CatquestVol2.ts
@@ -0,0 +1,49 @@
+enum songs {
+    //% block="Room" 
+    Jumpy = 0,
+}  
+
+
+namespace Music {
+    /**
+     * CatQuestVo2 
+     * 1 loopable songs
+     */
+    //% block
+    export function CatQuestVo2(melody: songs): void {
+        switch (melody) {
+            case songs.Jumpy:
+                timer.background(function () {
+                    for (let index = 0; index < 3; index++) {
+                        music.playMelody("C - C - C - C - ", 300)
+                        music.playMelody("D - D - D - D - ", 300)
+                    }
+                })
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B A C5 - B A C5 - ", 300)
+                music.playMelody("B B A - A B B C5 ", 300)
+                music.playMelody("B A C5 - C5 A B - ", 300)
+                music.playMelody("B B C5 C5 B B C5 C5 ", 300)
+                music.stopAllSounds()
+                music.playMelody("- - - - - - - - ", 300)
+                music.playMelody("C5 B G F D G A B ", 300)
+                timer.background(function () {
+                    for (let index = 0; index < 3; index++) {
+                        music.playMelody("C - C - C - C - ", 300)
+                        music.playMelody("D - D - D - D - ", 300)
+                    }
+                })
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B A C5 - B A C5 - ", 300)
+                music.playMelody("B A C5 - C5 A B - ", 300)
+                music.playMelody("B B A - A B B C5 ", 300)
+                music.playMelody("B B C5 C5 B B C5 C5 ", 300)
+                music.stopAllSounds()
+                music.playMelody("- - - - - - - - ", 300)
+                music.playMelody("C5 A A E D A A B ", 300)
+        }
+   }    
+}
+               
