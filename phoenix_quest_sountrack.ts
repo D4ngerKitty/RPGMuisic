277341c16d967273e316ca23960d0b9d9e2ebf8d
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2021-11-24 18:26:27 -0600

    Update pxt.json, phoenix_quest_sountrack.ts

---
+++ phoenix_quest_sountrack.ts
@@ -0,0 +1,49 @@
+enum Soundtrackenum {
+    //% block="Jumpy" 
+    Walking = 0,
+}
+
+
+
+namespace Music {
+    /**
+     * phoenix_quest
+     * 1 loopable songs
+     */
+    //% block
+    export function phoenix_quest_soundtrack(Soundtrack: Soundtrackenum): void {
+        switch (Soundtrack) {
+            case Soundtrackenum.Walking:
+                timer.background(function () {
+                    for (let index = 0; index < 3; index++) {
+                        music.playMelody("C - C - C - C - ", 300)
+                        music.playMelody("D - D - D - D - ", 300)
+                    }
+                })
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B A C5 - B A C5 - ", 300)
+                music.playMelody("B B A - A B B C5 ", 300)
+                music.playMelody("B A C5 - C5 A B - ", 300)
+                music.playMelody("B B C5 C5 B B C5 C5 ", 300)
+                music.stopAllSounds()
+                music.playMelody("- - - - - - - - ", 300)
+                music.playMelody("C5 B G F D G A B ", 300)
+                timer.background(function () {
+                    for (let index = 0; index < 3; index++) {
+                        music.playMelody("C - C - C - C - ", 300)
+                        music.playMelody("D - D - D - D - ", 300)
+                    }
+                })
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B B - - B B - - ", 300)
+                music.playMelody("B A C5 - B A C5 - ", 300)
+                music.playMelody("B A C5 - C5 A B - ", 300)
+                music.playMelody("B B A - A B B C5 ", 300)
+                music.playMelody("B B C5 C5 B B C5 C5 ", 300)
+                music.stopAllSounds()
+                music.playMelody("- - - - - - - - ", 300)
+                music.playMelody("C5 A A E D A A B ", 300)
+        }
+    }
+}
